807004ff3ea93b3b4a76bea0faa4578162ac65bd
Zaynn-Lea <gely.leandre@gmail.com>
2024-09-23 10:55:20 +0200

    functions before testing

--- PrettyCMD.c
+++ PrettyCMD.c
@@ -3,7 +3,35 @@
 #include <string.h>
 
 
-void BoxPrint()
+void WhiteLinePrint(int iNbrOfLines)
 {
-    
+    int i;
+    for (i = 0; i < iNbrOfLines; i++)
+        printf("\n");
+}
+
+
+void LinePrint(int iLength, char c)
+{
+    int i;
+    for (i = 0; i < iLength; i++)
+        printf(&c);
+}
+
+
+void BoxPrint(char str[])
+{
+    int i;
+    int iStrLength = sizeof(str) / sizeof(char);
+
+    // Displaying the Box :
+    printf("+");
+    LinePrint(iStrLength + 4, '-');
+    printf("+\n");
+
+    printf("|  %s  |\n", str);
+
+    printf("+");
+    LinePrint(iStrLength + 4, '-');
+    printf("+\n");
 }
--- PrettyCMD.h
+++ PrettyCMD.h
@@ -1,2 +1,8 @@
 
-void BoxPrint();
+void WhiteLinePrint(int iNbrOfLines);
+
+
+void LinePrint(int iLength, char c);
+
+
+void BoxPrint(char str[]);
