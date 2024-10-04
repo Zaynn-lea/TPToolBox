0b63cb9a0247766aa223e2794633affba98fbc43
Zaynn-Lea <gely.leandre@gmail.com>
2024-10-04 08:09:53 +0200

    added simple documentation

--- PrettyCMD.c
+++ PrettyCMD.c
@@ -1,9 +1,30 @@
 
+/*
+Module to make organising and displaying results or object in the command prompt easier and prettier in C
+
+Part of my toolbox library for the programing classes and maybe to help debug the mini projects
+
+----------------------------------------------------------------
+
+made by :
+    Gely Leandre : https://github.com/Zaynn-lea
+
+the :
+04 / 10 / 2024
+*/
+
 #include "PrettyCMD.h"
 
 
 void WhiteLinePrint(int iNbrOfLines)
 {
+    /*
+    Prints a bunch of white lines in the command prompt
+    For exemple, to space things out
+
+    iNbrOfLines is a positive integer
+    */
+    
     int i;
     for (i = 0; i < iNbrOfLines; ++i)
         printf("\n");
@@ -12,6 +33,13 @@ void WhiteLinePrint(int iNbrOfLines)
 
 void LinePrint(int iLength, char c)
 {
+    /*
+    Prints iLength times the character c in the command line
+
+    iLength is a positive integer
+    c is a single character
+    */
+
     int i;
     for (i = 0; i < iLength; ++i)
         printf("%c", c);
@@ -20,8 +48,16 @@ void LinePrint(int iLength, char c)
 
 void BoxPrint(char *str)
 {
+    /*
+    Display a boxed text in the command line as follows :
+    +-------+
+    |  str  |
+    +-------+
+
+    str is a pointer to a char (a string)
+    */
+
     size_t iStrLength = strlen(str);
-    printf("%d\n", iStrLength);
 
     // Displaying the Box :
     printf("+");
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # TP ToolBox
 
+A toolbox library for the programing classes and maybe to help debug the mini projects.
 
-A toolbox library for the programing classes and maybe to help debug the mini projects 
+## This library contains a few modules :
+
+- PrettyCMD :
+    PrettyCMD is a module to make dysplaying and organising the command prompt interface easier. For exemple, having utility to print arrays, matrices, boxes, make space, etc...
